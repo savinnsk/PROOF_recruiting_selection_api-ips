5064003a2cf80f792aad3d69140c160c6dca4f32
Savio Picanço <saviopbrito@outlook.com>
2022-04-10 07:13:40 -0300

    server started

---
+++ .history/src/server_20220410071204.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.use(express.json);
---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.use(express.json);
+
+app.listen(3000, console.log("server is running"));
